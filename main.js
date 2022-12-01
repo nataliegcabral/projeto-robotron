3892b51e4b072eadb9c38a8521f2ff008a59610d
Natalie Cabral <nataliegc23@gmail.com>
2022-12-01 14:56:29 -0300

    updates

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-
-
 //função anônima pode ser chamada por () => {} (arrow function)
-
 const controle = document.querySelectorAll('[data-controle]')
+
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) =>  {
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
         
         //textContent é o value dos textos
     })
 })
 
 function manipulaDados (operacao, controle) {
-    const peca = controle.querySelector('.controle-contador')
+    const peca = controle.querySelector('[data-contador]')
 
     if(operacao === "-") {
         peca.value = parseInt(peca.value) - 1  
--- praticando/script.js
+++
@@ -1,6 +0,0 @@
-const calcular = document.querySelector('#calcular');
-const resultado = document.querySelector('.resultado');
-
-calcular.addEventListener('click', (evento) => {
-    resultado.innerHTML = 'fui clicado'
-})
